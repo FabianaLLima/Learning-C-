e6b48b7b207ca2a3d95a5e70e99e2b842c65353b
FabianaLLima <fabianallima@hotmail.com>
2024-05-18 19:45:29 -0300

    Exercicico somando valores

---
+++ Exercicio aula 4 Somando valores/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio4
+{
+    //Classe
+    class Program
+    {
+        //Método principal
+        static void Main(string[] args) 
+        {
+            //Somar dois números 
+            int n1, n2, resultado;
+
+            //Obtendo os Valores 
+            Console.Write("Digite o número 1: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número 2: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            //Processar
+            resultado = n1 + n2;
+
+            // Exibir
+            Console.WriteLine("A soma de {0} + {1} é = {2}", n1, n2, resultado);
+
+        }
+    }
+} 
