c4446d4ce25acab4a62f94e39bdb48bec1de3a7a
FabianaLLima <fabianallima@hotmail.com>
2024-05-18 19:39:37 -0300

    Exercicio 1 c#

---
+++ PrimeiroPrograma/ExercicioConsoleC#/ExercicioConsoleC#/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ExercicioConsoleC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome:Fabiana");
+            Console.WriteLine("Telefone: 11949494060");
+            Console.WriteLine("E-mail: fabianallima@hotmail.com");
+            Console.ReadLine();
+        }
+    }
+}
