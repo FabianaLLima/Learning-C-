df1a175b7f9312fb06f38f7a930cf6c96cfa1b96
FabianaLLima <fabianallima@hotmail.com>
2024-05-18 19:46:00 -0300

    Exercicio multiplicação de valores

---
+++ Exercicio Aula 5 Multiplicando/Exercicio5 Multiplicando/Exercicio5 Multiplicando/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio5 
+{
+    class Program
+    {
+
+        static void Main(string[] args) 
+        {
+            //Obter o valores
+            Console.WriteLine("Digite o valor do produto: ");
+            decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade: ");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            //Calcular
+            decimal resultado = quantidade * valor;
+
+            //Exibir (esse {0:C} é para converter para moeda
+            Console.WriteLine("Total da compra: {0:C}", resultado);
+            Console.ReadLine();
+                
+        }
+    }
+
+}
+
