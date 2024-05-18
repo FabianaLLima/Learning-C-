c4659cc6580d2ade31132e89b8d530103ef78df0
FabianaLLima <fabianallima@hotmail.com>
2024-05-18 19:44:35 -0300

    Exercício 2

---
+++ Exercicio Console App2/Exercício Console App2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua Idade:");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            int dias = idade * 365;
+
+            Console.WriteLine("Olá {0}, você já viveu {1} dias. ", nome, dias);
+            Console.ReadLine();
+
+        }
+    }
+ }
+
+
